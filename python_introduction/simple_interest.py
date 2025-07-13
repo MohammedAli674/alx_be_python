bcb15911044adaea23e5615efe427d53a9cac2f3
Mohammed Ali Mahmoud <mohammedelhawy200@gmail.com>
2025-07-13 14:21:00 +0300

    Create simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+principal = 1000      # Principal amount in dollars
+rate = 0.05           # Annual interest rate (5%)
+time = 3              # Time in years
+interest = principal * rate * time
+print (f"For principal = 1000, rate = 0.05, and time = 3, the output should be: {interest}")
+print(f"The simple interest is: {interest}")
+
+
+
+Add basic_operations.py to python_introduction folder
